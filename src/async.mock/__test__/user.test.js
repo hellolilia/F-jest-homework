a8f4b6cd0d751e8c96bd765fdd40a57c1d920669
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-11-14 19:37:08 +0800

    completed jest homework

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,4 +1,5 @@
-import { register } from "../user";
+import { verifyUsername } from "../verify";
+import { validate, register } from "../user";
 
 jest.mock("../verify");
 jest.mock("axios");
@@ -6,9 +7,14 @@ jest.mock("axios");
 describe("register", () => {
   test("should post user when validated", () => {
     // TODO 19: add test here
+    const result = validate("userName", "password");
+    expect(result).toBeTruthy();
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockReturnValue(false);
+    const result = register("userName", "password");
+    await expect(result).rejects.toThrow("wrong username or password");
   });
 });
